9638c741595e810a04ebf7664220f18dfc47d3c8
nugasoft <nugasoft@gmail.com>
2023-11-23 18:40:48 -0600

    helmet

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mysql = require("mysql");
+const helmet = require("helmet");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(helmet());
 
 // Crear la conexión a la base de datos
 const db = mysql.createConnection({
@@ -23,32 +25,14 @@ db.connect((err) => {
 // Endpoint para crear un nuevo contacto
 app.post("/directorio", (req, res) => {
 	const { nombre, celular, correo } = req.body;
-	const query = `INSERT INTO directorio (nombre, celular, correo, fechacreo) VALUES ('${nombre}', '${celular}', '${correo}')`;
-	db.query(query, (err, result) => {
-		if (err) throw err;
-		res.status(201).send(`Contacto agregado con ID: ${result.insertId}`);
-	});
-});
-
-// Endpoint para listar todos los directorio
-app.get("/directorio", (req, res) => {
-	const query = "SELECT * FROM directorio";
-	db.query(query, (err, results) => {
-		if (err) throw err;
-		res.json(results);
-	});
-});
-
-// Endpoint para obtener un contacto específico por ID
-app.get("/directorio/:id", (req, res) => {
-	const query = "SELECT * FROM directorio WHERE id = " + req.params.id;
-
-	db.query(query, (err, result) => {
+	const query =
+		"INSERT INTO directorio (nombre, celular, correo) VALUES (?, ?, ?)";
+	db.query(query, [nombre, celular, correo], (err, result) => {
 		if (err) {
 			console.error(err.message);
 			res.status(500).json({ error: "Error interno del servidor" });
 		} else {
-			res.json(result);
+			res.status(201).send(`Contacto agregado con ID: ${result.insertId}`);
 		}
 	});
 });
@@ -56,19 +40,28 @@ app.get("/directorio/:id", (req, res) => {
 // Endpoint para actualizar un contacto
 app.put("/directorio/:id", (req, res) => {
 	const { nombre, celular, correo } = req.body;
-	const query = `UPDATE directorio SET nombre = '${nombre}', celular = '${celular}', correo = '${correo}' WHERE id = ${req.params.id}`;
-	db.query(query, (err, result) => {
-		if (err) throw err;
-		res.send(`Contacto con ID: ${req.params.id} actualizado`);
+	const query =
+		"UPDATE directorio SET nombre = ?, celular = ?, correo = ? WHERE id = ?";
+	db.query(query, [nombre, celular, correo, req.params.id], (err, result) => {
+		if (err) {
+			console.error(err.message);
+			res.status(500).json({ error: "Error interno del servidor" });
+		} else {
+			res.send(`Contacto con ID: ${req.params.id} actualizado`);
+		}
 	});
 });
 
 // Endpoint para eliminar un contacto
 app.delete("/directorio/:id", (req, res) => {
-	const query = `DELETE FROM directorio WHERE id = ${req.params.id}`;
-	db.query(query, (err, result) => {
-		if (err) throw err;
-		res.send(`Contacto con ID: ${req.params.id} eliminado`);
+	const query = "DELETE FROM directorio WHERE id = ?";
+	db.query(query, [req.params.id], (err, result) => {
+		if (err) {
+			console.error(err.message);
+			res.status(500).json({ error: "Error interno del servidor" });
+		} else {
+			res.send(`Contacto con ID: ${req.params.id} eliminado`);
+		}
 	});
 });
 
